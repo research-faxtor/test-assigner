be7a90f1075885c6a8704f9cfa46253459d64143
research-faxtor <research@faxtor.id>
2026-07-06 02:02:54 +0700

    Update route.ts

--- src/app/api/events/[id]/route.ts
+++ src/app/api/events/[id]/route.ts
@@ -1,12 +1,16 @@
 import { NextResponse } from "next/server";
-
 export async function POST(
   _req: Request,
   { params }: { params: Promise<{ id: string }> },
 ) {
   const { id } = await params;
   const userId = id;
-  if (userId == "normingftpi2" || userId == "ujicobaflsi") {
+  if (
+    userId == "normingftpi2" ||
+    userId == "ujicobaflsi" ||
+    userId == "SelarasMale" ||
+    userId == "SelarasFemale"
+  ) {
     return NextResponse.json({ message: "valid" });
   }
   return NextResponse.json({ error: "invalid" }, { status: 500 });

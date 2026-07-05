4cde7d7f4b488f3213d7e5898d5a15374d72aed3
research-faxtor <research@faxtor.id>
2026-07-06 02:31:42 +0700

    Update appwrite_client.ts

--- src/lib/appwrite_client.ts
+++ src/lib/appwrite_client.ts
@@ -1,9 +1,11 @@
 import { Client } from "appwrite";
-
 const client = new Client();
-
 client
   .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string)
   .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string);
+
+if (process.env.APPWRITE_API_KEY) {
+  client.setKey(process.env.APPWRITE_API_KEY as string);
+}
 
 export default client;
